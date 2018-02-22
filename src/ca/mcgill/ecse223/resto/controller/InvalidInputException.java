5a7567b91533cb31c76e86a686d03cea462b8cf4
Anudruth Manjunath <anudruth@gmail.com>
2018-02-22 16:16:19 -0500

    check

--- src/ca/mcgill/ecse223/resto/controller/InvalidInputException.java
+++ src/ca/mcgill/ecse223/resto/controller/InvalidInputException.java
@@ -1,19 +1,9 @@
 package ca.mcgill.ecse223.resto.controller;
-/**
- * @author anudruth
- *
- */
 
 public class InvalidInputException extends Exception {
 	
-
-	
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 6401374489868897442L;
-
+	
 	public InvalidInputException(String errorMessage) {
 		super(errorMessage);
 	}
