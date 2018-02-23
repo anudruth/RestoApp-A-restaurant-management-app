e31f853433999c34492ae0c490b8291b52cdf8c3
eliottbourachot <eliott.bourachot@gmail.com>
2018-02-23 16:05:49 -0500

    added removeTable() in Controller

--- src/ca/mcgill/ecse223/resto/controller/RestoAppController.java
+++ src/ca/mcgill/ecse223/resto/controller/RestoAppController.java
@@ -24,6 +24,45 @@ public class RestoAppController {
 	public RestoAppController() {
 	}
 	
+	public static void removeTable(int number) throws InvalidInputException {
+		RestoApp restoApp = RestoAppApplication.getRestoapp();
+		Table table = Table.getWithNumber(number);
+		if(table == null) throw new InvalidInputException("Invalid Table");
+		boolean reserved = table.hasReservations();
+		if(reserved == true) throw new InvalidInputException("Table is reserved"); //if table reserved throw exception
+		try {
+			List<Order> currentOrders = restoApp.getCurrentOrders();
+			for(Order order : currentOrders) { //if table in use throw exception
+				List<Table> tables = order.getTables();
+				boolean inUse = tables.contains(table);
+				if(inUse == true) throw new InvalidInputException("Table is in use");
+			}
+			restoApp.removeCurrentTable(table);
+			RestoAppApplication.save();
+		}catch (RuntimeException e){
+			throw new InvalidInputException(e.getMessage());
+		}
+	}
+	
+	public static void removeTable(Table table) throws InvalidInputException {
+		RestoApp restoApp = RestoAppApplication.getRestoapp();
+		if(table == null) throw new InvalidInputException("Invalid Table");
+		boolean reserved = table.hasReservations();
+		if(reserved == true) throw new InvalidInputException("Table is reserved"); //if table reserved throw exception
+		try {
+			List<Order> currentOrders = restoApp.getCurrentOrders();
+			for(Order order : currentOrders) { //if table in use throw exception
+				List<Table> tables = order.getTables();
+				boolean inUse = tables.contains(table);
+				if(inUse == true) throw new InvalidInputException("Table is in use");
+			}
+			restoApp.removeCurrentTable(table);
+			RestoAppApplication.save();
+		}catch (RuntimeException e){
+			throw new InvalidInputException(e.getMessage());
+		}
+	}
+	
 	//code for create/delete operation should look something like this
 //	public static void createTable(int number, int x, int y, int width, int length, int numberOfSeats) throws InvalidInputException {
 //		RestoApp restoApp = RestoAppApplication.getRestoApp();
