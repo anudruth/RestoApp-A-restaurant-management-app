12f438bd5985581cf91117afd48d7136f1815dd4
eliottbourachot <eliott.bourachot@gmail.com>
2018-04-08 14:17:06 -0400

    removed some unused imports

--- src/ca/mcgill/ecse223/resto/application/RestoAppApplication.java
+++ src/ca/mcgill/ecse223/resto/application/RestoAppApplication.java
@@ -4,21 +4,7 @@
 package ca.mcgill.ecse223.resto.application;
 
 
-import java.sql.Date;
-import java.sql.Time;
-import java.util.List;
-
-import ca.mcgill.ecse223.resto.controller.InvalidInputException;
-import ca.mcgill.ecse223.resto.controller.RestoAppController;
-import ca.mcgill.ecse223.resto.model.Menu;
-import ca.mcgill.ecse223.resto.model.MenuItem;
-import ca.mcgill.ecse223.resto.model.Order;
-import ca.mcgill.ecse223.resto.model.OrderItem;
-import ca.mcgill.ecse223.resto.model.PricedMenuItem;
 import ca.mcgill.ecse223.resto.model.RestoApp;
-import ca.mcgill.ecse223.resto.model.Seat;
-import ca.mcgill.ecse223.resto.model.Table;
-import ca.mcgill.ecse223.resto.model.Table.Status;
 import ca.mcgill.ecse223.resto.persistence.PersistenceObjectStream;
 import ca.mcgill.ecse223.resto.view.RestoAppPage;
 
--- src/ca/mcgill/ecse223/resto/view/RestoAppPage.java
+++ src/ca/mcgill/ecse223/resto/view/RestoAppPage.java
@@ -17,7 +17,6 @@ import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
